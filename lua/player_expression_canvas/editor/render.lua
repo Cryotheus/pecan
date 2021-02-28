ccd50c08c6713bea7e59eec8b955e5734e5abe70
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2021-02-28 13:45:52 -0500

    Camera movement and floor

--- lua/player_expression_canvas/editor/render.lua
+++ lua/player_expression_canvas/editor/render.lua
@@ -1,15 +1,63 @@
+local camera_angles
+local camera_angle_sensitivty = 0.25
+local camera_angles_start
+local camera_position
+local camera_position_sensitivty = 60
+local material_floor = Material("gui/alpha_grid.png")
+
+--pecan functions
 function PECAN:PecaneCreateModel()
-	local model = ClientsideModel(LocalPlayer():GetModel(), RENDERGROUP_OTHER)
+	local local_player = LocalPlayer()
+	local model = ClientsideModel(local_player:GetModel(), RENDERGROUP_OTHER)
 	
 	model:SetAngles(Angle(0, 180, 0))
 	
-	function model:GetPlayerColor() return Vector(1, 0, 0) end
+	function model:GetPlayerColor() return local_player:GetPlayerColor() end
 	
 	return model
 end
 
 function PECAN:PecaneRender(editor, width, height)
-	cam.Start3D(Vector(-100, 0, 36), angle_zero)
+	cam.Start3D(camera_position, camera_angles)
+		render.SuppressEngineLighting(true)
+		
+		cam.Start3D2D(vector_origin, Angle(0, 0, 0), 0.125)
+			render.Clear(0, 0, 0, 0)
+			
+			surface.SetMaterial(material_floor)
+			surface.SetDrawColor(64, 64, 64)
+			surface.DrawTexturedRect(-512, -512, 1024, 1024)
+		cam.End3D2D()
+		
 		self.EditorModel:DrawModel()
+		
+		render.SuppressEngineLighting(false)
 	cam.End3D()
 end
+
+function PECAN:PecaneTranslateAngles(editor, x, y)
+	local pitch = y * camera_angle_sensitivty
+	local yaw = x * -camera_angle_sensitivty
+	
+	camera_angles_calc = Angle(pitch, yaw, 0)
+	
+	--camera_angles = select(2, LocalToWorld(vector_origin, camera_angles_start, vector_origin, camera_angles_calc))
+	camera_angles = camera_angles_start + camera_angles_calc
+	camera_angles.pitch = math.Clamp(camera_angles.pitch, -89, 89)
+	
+	camera_angles:Normalize()
+end
+
+function PECAN:PecaneTranslateAnglesFinish(editor) camera_angles_start = camera_angles end
+
+function PECAN:PecaneTranslatePosition(editor, think_time, translation)
+	--more?
+	camera_position = LocalToWorld(translation * camera_position_sensitivty * think_time, angle_zero, camera_position, camera_angles)
+end
+
+--hooks
+hook.Add("PecaneOpen", "pecan_editor", function()
+	camera_position = Vector(-40, 0, 60)
+	camera_angles = angle_zero
+	camera_angles_start = angle_zero
+end)
--- lua/player_expression_canvas/panels/editor.lua
+++ lua/player_expression_canvas/panels/editor.lua
@@ -1,11 +1,91 @@
+--local variables
+local binds = {}
+local call_translate_position = false
+local cursor_start_x, cursor_start_y
+local cursor_translate_x, cursor_translate_y
 local PANEL = {}
+local speed = 1
+local translate_position = vector_origin
+local think_time = RealTime()
+local thinking_binds = {}
 
+--local tables
 local default_frames = {
 	{
 		class = "TextureEditor",
-		pos = {0, 0}
+		pos = {ScrW() - 512, 0}
 	}
 }
+
+local bind_press_functions = {
+	["+attack"] = function(self)
+		cursor_start_x, cursor_start_y = ScrW() * 0.5, ScrH() * 0.5
+		
+		RememberCursorPosition()
+		input.SetCursorPos(cursor_start_x, cursor_start_y)
+		self:SetCursor("blank")
+	end,
+	
+	toggleconsole = function(self)
+		self:Remove()
+		
+		gui.ActivateGameUI()
+	end
+}
+
+local bind_release_functions = {
+	["+attack"] = function(self)
+		RestoreCursorPosition()
+		
+		hook.Call("PecaneTranslateAnglesFinish", PECAN, self)
+		
+		self:SetCursor("arrow")
+	end
+}
+
+local bind_think_functions = {
+	["+attack"] = function(self)
+		--more!
+		self:SetCursor("blank")
+		
+		hook.Call("PecaneTranslateAngles", PECAN, self, gui.MouseX() - cursor_start_x, gui.MouseY() - cursor_start_y)
+	end,
+	
+	["+back"] = function(self)
+		call_translate_position = true
+		translate_position = translate_position - Vector(speed, 0, 0)
+	end,
+	
+	["+forward"] = function(self)
+		call_translate_position = true
+		translate_position = translate_position + Vector(speed, 0, 0)
+	end,
+	
+	["+jump"] = function(self)
+		call_translate_position = true
+		translate_position = translate_position + Vector(0, 0, speed)
+	end,
+	
+	["+moveleft"] = function(self)
+		call_translate_position = true
+		translate_position = translate_position + Vector(0, speed, 0)
+	end,
+	
+	["+moveright"] = function(self)
+		call_translate_position = true
+		translate_position = translate_position - Vector(0, speed, 0)
+	end
+}
+
+--local functions
+local function bind_call(self, code, press)
+	local bind = input.LookupKeyBinding(code)
+	
+	if bind then
+		if press then self:OnBindPressed(bind)
+		else self:OnBindReleased(bind) end
+	end
+end
 
 --panel functions
 function PANEL:Init()
@@ -14,12 +94,6 @@ function PANEL:Init()
 	if exiting_panel then exiting_panel:Remove() end
 	
 	self.HeaderHeight = 24
-	
-	self:SetFocusTopLevel(true)
-	self:SetName("PecanEditor")
-	self:SetParent(GetHUDPanel())
-	self:SetPos(0, 0)
-	self:SetSize(ScrW(), ScrH())
 	
 	do --header
 		local header = vgui.Create("DPanel", self)
@@ -47,39 +121,77 @@ function PANEL:Init()
 	
 	do --default frames
 		for index, frame_data in ipairs(default_frames) do
-			local frame = vgui.Create("Pecan" .. frame_data.class, self)
+			local class = "Pecan" .. frame_data.class
 			
-			frame:SetKeyboardInputEnabled(true)
-			frame:SetMouseInputEnabled(true)
-			
-			if frame_data.pos then frame:SetPos(frame_data.pos) end
-			if frame_data.size then frame:SetSize(frame_data.size) end
+			if vgui.GetControlTable(class) then
+				local frame = vgui.Create(class, self)
+				
+				frame:SetKeyboardInputEnabled(true)
+				frame:SetMouseInputEnabled(true)
+				
+				if frame_data.pos then
+					local x, y = unpack(frame_data.pos)
+					
+					frame:SetPos(x, y + self.HeaderHeight)
+				else frame:SetPos(0, self.HeaderHeight) end
+				
+				if frame_data.size then frame:SetSize(unpack(frame_data.size)) end
+			end
 		end
 	end
 	
-	self:SetHeaderHeight(24)
+	--self:MouseCapture(true)
+	self:SetFocusTopLevel(true)
+	self:SetName("PecanEditor")
+	self:SetParent(GetHUDPanel())
+	self:SetPos(0, 0)
+	self:SetSize(ScrW(), ScrH())
 	
 	self:MakePopup()
 	self:DoModal()
 end
 
-function PANEL:OnKeyCodePressed(key_code)
-	print("key_code " .. key_code)
-	print("bound to " .. input.LookupKeyBinding(key_code))
+function PANEL:OnBindPressed(bind)
+	if bind_press_functions[bind] then bind_press_functions[bind](self) end
+	if bind_think_functions[bind] then thinking_binds[bind] = true end
+	
+	binds[bind] = true
 end
 
-function PANEL:OnKeyCodeReleased(key_code)
-	print("key_code " .. key_code)
-	print("bound to " .. input.LookupKeyBinding(key_code))
+function PANEL:OnBindReleased(bind, closing)
+	if bind_release_functions[bind] then bind_release_functions[bind](self, closing) end
+	
+	binds[bind] = nil
+	thinking_binds[bind] = nil
 end
 
-function PANEL:OnRemove() hook.Call("PecaneClose", PECAN) end
+function PANEL:OnKeyCodePressed(code) bind_call(self, code, true) end
+function PANEL:OnKeyCodeReleased(code) bind_call(self, code, false) end
+function PANEL:OnMousePressed(code) bind_call(self, code, true) end
+function PANEL:OnMouseReleased(code) bind_call(self, code, false) end
+
+function PANEL:OnRemove()
+	for bind in pairs(binds) do
+		if binds[bind] then
+			self:OnBindReleased(bind, true)
+			
+			binds[bind] = nil
+			thinking_binds[bind] = nil
+		end
+	end
+	
+	hook.Call("PecaneClose", PECAN)
+end
 
 function PANEL:Paint(width, height)
-	surface.SetDrawColor(20, 20, 20, 128)
+	local clipping = DisableClipping(true)
+	
+	surface.SetDrawColor(20, 20, 20, 255)
 	surface.DrawRect(0, 0, width, height)
 	
 	hook.Call("PecaneRender", PECAN, self, width, height)
+	
+	DisableClipping(clipping)
 end
 
 function PANEL:PerformLayout(width, height)
@@ -91,6 +203,24 @@ function PANEL:SetHeaderHeight(height)
 	self.Header:SetHeight(height)
 	
 	self.HeaderHeight = height
+end
+
+function PANEL:Think()
+	for bind in pairs(thinking_binds) do if bind_think_functions[bind] then bind_think_functions[bind](self) end end
+	
+	if call_translate_position then
+		local duck_mult = binds["+speed"] and 2 or 1
+		local speed_mult = binds["+duck"] and 0.2 or 1
+		
+		translate_position:Normalize()
+		
+		hook.Call("PecaneTranslatePosition", PECAN, self, RealTime() - think_time, translate_position * speed * duck_mult * speed_mult)
+		
+		call_translate_position = false
+		translate_position = vector_origin
+	end
+	
+	think_time = RealTime()
 end
 
 derma.DefineControl("PecanEditor", "Editor for Pecan", PANEL, "EditablePanel")
